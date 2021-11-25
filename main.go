035d84af4c7bab8e694bca5dbc71d69614d34060
sami sabir-idrissi <samisabiridrissi@icloud.com>
2021-11-25 01:53:19 -0500

    passing the http.HandlerFunc with pathHandler function into the listenAndServe function

--- main.go
+++ main.go
@@ -20,10 +20,7 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-type Router struct {
-}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func pathHandler(w http.ResponseWriter, r *http.Request) {
 
 	switch r.URL.Path {
 	case "/":
@@ -33,14 +30,34 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	default:
 		http.Error(w, "Page not found", http.StatusNotFound)
 	}
-
 }
+
+// type Router struct{}
+
+// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		http.Error(w, "Page not found", http.StatusNotFound)
+// 	}
+
+// }
 
 func main() {
 
-	var router Router
+	http.Handler - interface with the serverHTTP method
+	http.HandleFunc - a function type that accepts the same args as ServeHTTP method. also implements http.Handler
+
+	http.Handle("/", http.Handler)
+	http.HandleFunc("/", pathHandler)
+
+
 	fmt.Println("Starting the server on 3000:")
-	err := http.ListenAndServe(":3000", router)
+	err := http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
 	if err != nil {
 		panic(err)
 	}
