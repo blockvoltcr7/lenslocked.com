6f08d23d2354af0b190d6808f8464b1e44e9b92f
sami sabir-idrissi <samisabiridrissi@icloud.com>
2021-11-28 07:19:35 -0500

    working with template.parsefiles function and structs

---
+++ cmd/exp/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type User struct {
+	Name string
+	Age  int
+	Meta UserMeta
+}
+
+type UserMeta struct {
+	Visits int
+}
+
+func main() {
+
+	t, err := template.ParseFiles("hello.gohtml")
+
+	if err != nil {
+		panic(err)
+	}
+
+	// user := struct {
+	// 	Name string
+	// 	Age  int
+	// }{
+	// 	Name: "Sami Sabir-idrissi",
+	// 	Age:  32,
+	// }
+
+	user := User{
+		Name: "Sami Sabir-Idrissi",
+		Age:  32,
+		Meta: UserMeta{Visits: 4},
+	}
+
+	err = t.Execute(os.Stdout, user)
+
+	if err != nil {
+		panic(err)
+	}
+
+}
