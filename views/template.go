f6b38f9e5f9b5511bd0f97642d98cc0413c16ea0
sami sabir-idrissi <samisabiridrissi@icloud.com>
2021-12-05 11:40:47 -0500

    working on passing data to faq page via FAQ controller to the template

--- views/template.go
+++ views/template.go
@@ -42,7 +42,7 @@ func Parse(filepath string) (Template, error) {
 func (t Template) Execute(w http.ResponseWriter, data interface{}) {
 
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	err := t.htmlTpl.Execute(w, nil)
+	err := t.htmlTpl.Execute(w, data)
 	if err != nil {
 		log.Printf("executing template: %v", err)
 		http.Error(w, "there was an error executing the template",
