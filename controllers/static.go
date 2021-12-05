b303e6424ec3ef6d98f7663e0fc77d67b63fc686
sami sabir-idrissi <samisabiridrissi@icloud.com>
2021-12-05 00:50:59 -0500

    added controller and updated main.go to improve reusability and readablity

---
+++ controllers/static.go
@@ -0,0 +1,15 @@
+package controllers
+
+import (
+	"net/http"
+
+	"lenslocked.com/views"
+)
+
+func StaticHandler(tpl views.Template) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, nil)
+	}
+
+}
--- main.go
+++ main.go
@@ -2,67 +2,43 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
+	"lenslocked.com/controllers"
 	"lenslocked.com/views"
 )
-
-func executeTemplate(w http.ResponseWriter, filepath string) {
-
-	t, err := views.Parse(filepath)
-	if err != nil {
-		log.Printf("parsing template: %v", err)
-		http.Error(w, "there was an error parsing the templates",
-			http.StatusInternalServerError)
-		return
-	}
-
-	t.Execute(w, nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, "templates/home.gohtml")
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, "templates/contact.gohtml")
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, "templates/faq.gohtml")
-}
-
-// HTTP handler accessing the url routing parameters.
-func MyRequestHandler(w http.ResponseWriter, r *http.Request) {
-	// fetch the url parameter `"userID"` from the request of a matching
-	// routing pattern. An example routing pattern could be: /users/{userID}
-	userID := chi.URLParam(r, "userID")
-
-	// fetch `"key"` from the request context
-	ctx := r.Context()
-	key := ctx.Value("key").(string)
-
-	// respond to the client
-	w.Write([]byte(fmt.Sprintf("hi %v, %v", userID, key)))
-
-}
 
 func main() {
 
 	r := chi.NewRouter()
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
-	r.Get("/users", MyRequestHandler)
+
+	//parse the template
+	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/faq", controllers.StaticHandler(tpl))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
 
 	fmt.Println("Starting the server on 3000:")
-	err := http.ListenAndServe(":3000", r)
+	http.ListenAndServe(":3000", r)
 
 	if err != nil {
 		panic(err)
