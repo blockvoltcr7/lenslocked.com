8583a72cdfb0fc0a1370bda50d975e33a6882e5d
sami sabir-idrissi <samisabiridrissi@icloud.com>
2021-12-05 02:11:51 -0500

    added the fs embed class file system. created ParseFS function

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"lenslocked.com/controllers"
+	"lenslocked.com/templates"
 	"lenslocked.com/views"
 )
 
@@ -15,13 +15,13 @@ func main() {
 	r := chi.NewRouter()
 
 	//parse the template
-	tpl := views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))
+	tpl := views.Must(views.ParseFS(templates.FS, "home.gohtml"))
 	r.Get("/", controllers.StaticHandler(tpl))
 
-	tpl = views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))
+	tpl = views.Must(views.ParseFS(templates.FS, "contact.gohtml"))
 	r.Get("/contact", controllers.StaticHandler(tpl))
 
-	tpl = views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))
+	tpl = views.Must(views.ParseFS(templates.FS, "faq.gohtml"))
 	r.Get("/faq", controllers.StaticHandler(tpl))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
