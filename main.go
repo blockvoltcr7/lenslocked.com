ddd01d6e057f6163cad00bdff0b09e80f55cb578
sami sabir-idrissi <samisabiridrissi@icloud.com>
2021-12-05 01:06:05 -0500

    applied must function

--- main.go
+++ main.go
@@ -15,22 +15,13 @@ func main() {
 	r := chi.NewRouter()
 
 	//parse the template
-	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
-	if err != nil {
-		panic(err)
-	}
+	tpl := views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))
 	r.Get("/", controllers.StaticHandler(tpl))
 
-	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
-	if err != nil {
-		panic(err)
-	}
+	tpl = views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))
 	r.Get("/contact", controllers.StaticHandler(tpl))
 
-	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
-	if err != nil {
-		panic(err)
-	}
+	tpl = views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))
 	r.Get("/faq", controllers.StaticHandler(tpl))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
@@ -39,9 +30,5 @@ func main() {
 
 	fmt.Println("Starting the server on 3000:")
 	http.ListenAndServe(":3000", r)
-
-	if err != nil {
-		panic(err)
-	}
 
 }
