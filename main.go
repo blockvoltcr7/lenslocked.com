5415b1f54d1470f1121c7666f5ac44595b621207
sami sabir-idrissi <samisabiridrissi@icloud.com>
2021-12-08 22:00:03 -0500

    added the users.Create method to handle the signup form

--- main.go
+++ main.go
@@ -25,11 +25,9 @@ func main() {
 		"faq.gohtml", "tailwind.gohtml"))))
 
 	usersC := controllers.Users{}
-	usersC.Templates.New = views.Must(views.ParseFS(
-		templates.FS,
-		"signup.gohtml", "tailwind.gohtml",
-	))
-	r.Get("/signup", usersC.New)
+	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
+	r.Get("/signup", usersC.New)    //this is just getting the signup page
+	r.Post("/users", usersC.Create) //this post function that is inside the signup page
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
